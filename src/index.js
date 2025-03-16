8feb5dace1562c096c67549de5469c3e8d3ab983
stephen <stephenmagiya97@gmail.com>
2025-03-16 22:57:41 +0300

    adds my todos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskCreater = document.getElementById("create-task-form");
+  taskCreater.addEventListener("submit", logging);
 });
+function logging(event) {
+  event.preventDefault(); 
+
+  const input = document.getElementById("new-task-description").value;
+  if (!input.trim()) return; 
+
+  const todos = document.getElementById("tasks"); 
+  const li = document.createElement("li");
+  const btn = document.createElement("button");
+
+  li.textContent = input + " "; 
+  btn.textContent = "x";
+  btn.addEventListener("click", function () {
+    li.remove();
+  });
+
+  li.appendChild(btn);
+  todos.appendChild(li); // Append to the correct UL element
+
+  document.getElementById("create-task-form").reset(); // Clear input field
+}
